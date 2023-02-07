28a2e2e45b7aa110b324fa27ff1dabc48885a8ca
Quentin <39061148+LapsTimeOFF@users.noreply.github.com>
2023-02-07 21:33:13 +0100

    ci: removed release scripts

--- script/logChanglog.js
+++
@@ -1,5 +0,0 @@
-var conventionalChangelog = require('conventional-changelog');
-
-const config = require('conventional-changelog-metahub');
-conventionalChangelog({config})
-  .pipe(process.stdout); // or any writable stream
