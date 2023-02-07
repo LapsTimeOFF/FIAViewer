40cd4836696546030839d04967ef479760d41e3c
Quentin <39061148+LapsTimeOFF@users.noreply.github.com>
2023-02-07 20:10:25 +0100

    Remove useless code

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -64,7 +64,7 @@ async function createWindow() {
 
   // Test actively push message to the Electron-Renderer
   win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', new Date().toLocaleString())
+    // win?.webContents.send('main-process-message', new Date().toLocaleString())
   })
 
   // Make all links open with the browser, not with the application
