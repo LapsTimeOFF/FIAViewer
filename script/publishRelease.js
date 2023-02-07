5e5e990157cf44032864fef489ae989e69e65b44
Quentin <39061148+LapsTimeOFF@users.noreply.github.com>
2023-02-07 21:16:12 +0100

    ci(script): edit publishRelease.js

--- script/publishRelease.js
+++ script/publishRelease.js
@@ -14,28 +14,44 @@ const octokit = new Octokit({
 })
 
 const publish = async () => {
+    const {
+        data
+    } = await octokit.request('GET /repos/LapsTimeOFF/FIAViewer/releases', {
+        owner: 'LapsTimeOFF',
+        repo: 'FIAViewer'
+    });
+
+    console.log(data[0].id);
+
     var myStream = new Writable({
         write(chunk, encoding, callback) {
-            // Replace LapsTimeOFF and ChatGPT_Desktop by yours ofc
-            octokit.request('POST /repos/LapsTimeOFF/FIAViewer/releases', {
+            // octokit.request('POST /repos/LapsTimeOFF/FIAViewer/releases', {
+            //     owner: 'LapsTimeOFF',
+            //     repo: 'FIAViewer',
+            //     tag_name: `v${require('../package.json').version}`,
+            //     target_commitish: 'stable',
+            //     name: `v${require('../package.json').version}`,
+            //     body: chunk.toString(),
+            //     draft: true,
+            //     prerelease: false,
+            //     generate_release_notes: false
+            // })
+
+            octokit.request('PATCH /repos/LapsTimeOFF/FIAViewer/releases/{release_id}', {
                 owner: 'LapsTimeOFF',
                 repo: 'FIAViewer',
-                tag_name: `v${require('../package.json').version}`,
-                target_commitish: 'stable',
-                name: `v${require('../package.json').version}`,
+                release_id: data[0].id.toString(),
                 body: chunk.toString(),
-                draft: true,
-                prerelease: false,
-                generate_release_notes: false
+                draft: false
             })
 
 
             callback();
         }
     });
-    conventionalChangelog({
-        config
-    }).pipe(myStream)
+    // conventionalChangelog({
+    //     config
+    // }).pipe(myStream)
 
 }
 
