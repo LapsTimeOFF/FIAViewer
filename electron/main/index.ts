ba7f561070617ae9da78235ef6c3a90961f9ced3
Quentin <39061148+LapsTimeOFF@users.noreply.github.com>
2023-02-09 21:22:22 +0100

    ci: Fix release script

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -110,20 +110,3 @@ app.on("activate", () => {
     createWindow();
   }
 });
-
-// New window example arg: new windows url
-ipcMain.handle("open-win", (_, arg) => {
-  const childWindow = new BrowserWindow({
-    webPreferences: {
-      preload,
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  });
-
-  if (process.env.VITE_DEV_SERVER_URL) {
-    childWindow.loadURL(`${url}#${arg}`);
-  } else {
-    childWindow.loadFile(indexHtml, { hash: arg });
-  }
-});
---
+++ script/logChangelog.js
@@ -0,0 +1,7 @@
+var conventionalChangelog = require('conventional-changelog');
+
+const config = require('conventional-changelog-metahub');
+conventionalChangelog({
+        config
+    })
+    .pipe(process.stdout); // or any writable stream
